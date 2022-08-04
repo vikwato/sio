68a79f49ce1fd0ece0f8f821ee2e7684c503f2ca
jomlink <jonasmwansa7@gmail.com>
2022-08-04 06:13:03 -0700

    Jonas mwansa

--- hello-world.py
+++ hello-world.py
@@ -2,4 +2,4 @@ print("My name is Vikwato Kamanga.\n"
       "I am currently pursuing a Masters of Science Degree in Analytics from Georgia Insititute of Technology\n"
       "I enjoy programming, data management and system administration\n"
       "Outside work, I enjoy watching and playing sport, listening to music, playing video games and watching TV.")
-print("My name is Jonas mwansa, am an aspiring full stack developer")
+print("My name is Jonas mwansa, am an aspiring full stack developer, I am a recent computer Engineering graduae")
