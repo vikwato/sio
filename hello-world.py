7cabf61729a823c78bb72a4dc3aacd4ff0816dd0
Stephenlungo <45937346+Stephenlungo@users.noreply.github.com>
2022-08-04 14:25:03 +0200

    Update hello-world.py
    First commit

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,4 @@
-print("My name is Vikwato Kamanga.\n"
-      "I am currently pursuing a Masters of Science Degree in Analytics from Georgia Insititute of Technology\n"
-      "I enjoy programming, data management and system administration\n"
-      "Outside work, I enjoy watching and playing sport, listening to music, playing video games and watching TV.")
+print("My name is Stephen Lungo.\n"
+      "I hold a bachelors Degree ii Information Technology\n"
+      
+     )
