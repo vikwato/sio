6612d0f9ab947731fb325957be793b9614f0ce19
Andrew Chiziba <chizibaandrew@gmail.com>
2022-08-04 13:26:55 +0200

    Andrew Chiziba

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,3 @@
-print("My name is Vikwato Kamanga.\n"
-      "I am currently pursuing a Masters of Science Degree in Analytics from Georgia Insititute of Technology\n"
-      "I enjoy programming, data management and system administration\n"
-      "Outside work, I enjoy watching and playing sport, listening to music, playing video games and watching TV.")
+print("Andrew's Branch.\n"
+"Details will be added later")
+      
